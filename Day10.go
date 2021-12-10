c41951010a7f09d5b2ee4c529c20b1b9bdf6326c
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-12-10 10:19:15 -0600

    Did Day 10

---
+++ Day10.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
+func main() {
+	fmt.Println(oneDay10(), twoDay10())
+}
+
+func oneDay10() int {
+	input, _ := ioutil.ReadFile("input10.txt")
+	var arr = strings.Split(string(input), "\n")
+	res := 0
+
+	for _, line := range arr {
+		stack := []string{}
+		for i := 0; i < len(line); i++ {
+			char := string(line[i])
+			if char == "(" || char == "[" || char == "{" || char == "<" {
+				stack = append(stack, char)
+			} else {
+				pop := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if char == ")" && pop != "(" {
+					res += 3
+					break
+				} else if char == "]" && pop != "[" {
+					res += 57
+					break
+				} else if char == "}" && pop != "{" {
+					res += 1197
+					break
+				} else if char == ">" && pop != "<" {
+					res += 25137
+					break
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func twoDay10() int {
+	input, _ := ioutil.ReadFile("input10.txt")
+	var arr = strings.Split(string(input), "\n")
+	res := []int{}
+
+	for _, line := range arr {
+		stack := []string{}
+		shouldBreak := false
+		for i := 0; i < len(line); i++ {
+			char := string(line[i])
+			if char == "(" || char == "[" || char == "{" || char == "<" {
+				stack = append(stack, char)
+			} else {
+				pop := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if char == ")" && pop != "(" {
+					shouldBreak = true
+				} else if char == "]" && pop != "[" {
+					shouldBreak = true
+				} else if char == "}" && pop != "{" {
+					shouldBreak = true
+				} else if char == ">" && pop != "<" {
+					shouldBreak = true
+				}
+			}
+			if shouldBreak {
+				break
+			}
+		}
+		if !shouldBreak && len(stack) != 0 {
+			res = append(res, 0)
+
+			for len(stack) != 0 {
+				res[len(res)-1] *= 5
+				pop := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				if pop == "(" {
+					res[len(res)-1] += 1
+				} else if pop == "[" {
+					res[len(res)-1] += 2
+				} else if pop == "{" {
+					res[len(res)-1] += 3
+				} else {
+					res[len(res)-1] += 4
+				}
+			}
+		}
+	}
+	sort.Ints(res)
+
+	return res[len(res)/2]
+}
