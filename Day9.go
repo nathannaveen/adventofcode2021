d76090c1f4064166fe6e44b7acc5aec65410b7e5
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-12-09 18:48:34 -0600

    Edited Day 9

--- Day9.go
+++ Day9.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	oneDay9()
 	twoDay9()
+	twoDay9Edit()
 }
 
 func oneDay9() {
@@ -97,4 +98,64 @@ func twoDay9() {
 	sort.Ints(res)
 
 	fmt.Println(res[len(res)-1] * res[len(res)-2] * res[len(res)-3]) // Last
+}
+
+func twoDay9Edit() {
+
+	/*
+		Edited m and res, so it is easier to understand.
+	*/
+
+	input, _ := ioutil.ReadFile("input9.txt")
+	var arr = strings.Split(string(input), "\n")
+
+	stack := []lowPos{}
+	m := make(map[pos]int) // position : size
+	visited := make(map[pos]bool)
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr[0]); j++ {
+			isLow := func(y, x int) bool {
+				// I put this function inside oneDay9() because I was to lazy to parse i, j, and arr
+				if y >= 0 && x >= 0 && y < len(arr) && x < len(arr[0]) {
+					return arr[i][j] < arr[y][x]
+				}
+				return true
+			}
+			if isLow(i+1, j) && isLow(i-1, j) && isLow(i, j+1) && isLow(i, j-1) {
+				stack = append(stack, lowPos{pos{i, j}, pos{i, j}})
+			}
+		}
+	}
+
+	canFlow := func(i, j, val int, start pos) {
+		// This is for finding whether water can flow from one place to another.
+		if i >= 0 && j >= 0 && i < len(arr) && j < len(arr[0]) &&
+			!visited[pos{i, j}] && arr[i][j] != '9' && int(arr[i][j]-'0') > val {
+			visited[pos{i, j}] = true
+			m[start]++
+			stack = append(stack, lowPos{pos{i, j}, start})
+		}
+	}
+
+	for len(stack) != 0 {
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		i, j, start := pop.actual.i, pop.actual.j, pop.start
+
+		canFlow(i+1, j, int(arr[i][j]-'0'), start)
+		canFlow(i-1, j, int(arr[i][j]-'0'), start)
+		canFlow(i, j+1, int(arr[i][j]-'0'), start)
+		canFlow(i, j-1, int(arr[i][j]-'0'), start)
+	}
+
+	res := []int{}
+
+	for _, val := range m {
+		res = append(res, val)
+	}
+
+	sort.Ints(res)
+
+	fmt.Println(res[len(res)-1] * res[len(res)-2] * res[len(res)-3])
 }
